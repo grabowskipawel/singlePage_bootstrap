0401ee69f1f2d7088fa9146c64e83f310663238d
grabowskipawel <grabowskipawel91@gmail.com>
2017-04-12 20:26:11 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,61 @@
+window.onload = function(){
+$(document).ready(function() {
+    
+  var getMax = function(){
+    return $(document).height() - $(window).height();
+  }
+  //zamiast document można użyć rozmiarów diva, sekcji.
+    
+  var getValue = function(){
+    return $(window).scrollTop();
+  }
+    
+  if ('max' in document.createElement('progress')) {
+    // Browser supports progress element
+    var progressBar = $('progress');
+        
+    // Set the Max attr for the first time
+    progressBar.attr({ max: getMax() });
+
+    $(document).on('scroll', function(){
+      // On scroll only Value attr needs to be calculated
+      progressBar.attr({ value: getValue() });
+    }); 
+      
+    $(window).resize(function(){
+      // On resize, both Max/Value attr needs to be calculated
+      progressBar.attr({ max: getMax(), value: getValue() });
+    }); 
+  
+  } else {
+
+    var progressBar = $('.progress-bar'), 
+        max = getMax(), 
+        value, width;
+        
+    var getWidth = function() {
+      // Calculate width in percentage
+      value = getValue();            
+      width = (value/max) * 100;
+      width = width + '%';
+      return width;
+    }
+        
+    var setWidth = function(){
+      progressBar.css({ width: getWidth() });
+    }
+        
+    $(document).on('scroll', setWidth);
+    $(window).on('resize', function(){
+      // Need to reset the Max attr
+      max = getMax();
+      setWidth();
+    });
+  }
+    
+    $('body').scrollspy({ target: '#navBar' }) //scrollspy
+  
+    
+});
+    document.getElementById
+}
